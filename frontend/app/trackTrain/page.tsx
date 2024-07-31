2c14ad0a798bba2387951336f92ba9e7c3e30d77
Tealspider23 <amitbehera204@gmail.com>
2024-07-31 20:29:29 +0530

    recommit url

--- frontend/app/trackTrain/page.tsx
+++ frontend/app/trackTrain/page.tsx
@@ -59,8 +59,8 @@ const TrackTrainPage: React.FC = () => {
 
   const handleSimulationToggle = async () => {
     const url = simulationRunning
-      ? 'https://realtime-rail-safety-simulation-alerts-application-4rjxhbdpb.vercel.app/stop-simulation'
-      : 'https://realtime-rail-safety-simulation-alerts-application-4rjxhbdpb.vercel.app/start-simulation';
+      ? 'https://realtime-rail-safety-simulation-alerts-application-kdsec2d9i.vercel.app/stop-simulation'
+      : 'https://realtime-rail-safety-simulation-alerts-application-kdsec2d9i.vercel.app/start-simulation';
 
     try {
       const response = await fetch(url, {
